0625a5ec105bbab23e0f056cd4680c0ac200e9c2
Jenise Ruehle <jela07@gmail.com>
2020-08-13 14:33:51 +0000

    Automatically backed up by Learn

--- db/migrate/20200813142954_create_movies.rb
+++ db/migrate/20200813142954_create_movies.rb
@@ -1,4 +1,9 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
+    create_table :movies do |t|
+      t.string :title 
+      t.integer :release_date 
+      t.string :director
+      
   end
 end
